b7d938b58700a2ea607eb570b51cab753ebada1c
digregorio <grehgcl@hotmail.com>
2022-03-20 03:03:41 -0300

    first commit

---
+++ questao4.js
@@ -0,0 +1,20 @@
+let carro = 110
+let caminhao = 80
+let cidades = 100
+let tempoViagemCaminhao = cidades / caminhao
+let tempoViagemCarro = cidades / carro
+let velocidadeCarro = 110
+let velocidadeCaminhao = 80
+
+let paradaCaminhao = tempoViagemCaminhao - (2*5)
+
+
+
+
+console.log(tempoViagemCaminhao)
+console.log(tempoViagemCarro)
+console.log(paradaCaminhao)
+
+
+
+
---
+++ questao5.js
@@ -0,0 +1,4 @@
+let nome = "greg"
+
+
+console.log(nome[3],nome[2],nome[1], nome[0])
